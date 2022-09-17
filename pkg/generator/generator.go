fbae8f5f01e4664ada6d9c14b287290ddc679d5a
Muhammadkhon <sartojtech@gmail.com>
2022-09-17 22:17:45 +0300

    Unique id generator added.

--- pkg/generator/generator.go
+++ pkg/generator/generator.go
@@ -1,2 +1,25 @@
 package generator
 
+func NewID(i int) (id string) {
+	var c int
+	var res []int
+	l := len(list)
+	if i <= l {
+		return list[i]
+	}
+	for {
+		c = i % l
+		i /= l
+
+		res = append(res, c, i)
+		if i < l {
+			break
+		}
+	}
+
+	for j := len(res) - 1; j >= 0; j-- {
+		id += list[res[j]]
+	}
+
+	return
+}
